54d4b8e19426ab4fe5563c52650d8b4444f67c5d
bluejoyq <bluejoyq@naver.com>
2019-11-03 06:22:29 +0900

    add doc of Application.h

--- Application.h
+++ Application.h
@@ -4,105 +4,95 @@ class Application {
 public:
 
 	/**
-	*	@brief
-	*	@pre
-	*	@post
-	*	@return
+	*	@brief	기본 생성자입니다.
+	*	@post	open = false
 	*/
 	Application();
 
 	/**
-	*	@brief
-	*	@pre
-	*	@post
-	*	@return
+	*	@brief	관리 프로그램 시작
+	*	@pre	open의 값이 설정되있어야 합니다.
+	*	@post	open값에 따라 다른 메뉴 실행
 	*/
 	void run();
 
 	/**
-	*	@brief
-	*	@pre
-	*	@post
-	*	@return
+	*	@brief	명령어를 입력받는 함수
+	*	@post	m_Command가 입력한 값이 된다.
+	*	@return	성공시 true 실패시 false
 	*/
-	bool GetCommand();
+	bool getCommand();
 
 	/**
-	*	@brief
-	*	@pre
-	*	@post
-	*	@return
+	*	@brief	open 여부를 리턴한다.
+	*	@return	open 리턴
 	*/
-	bool Open();
+	bool isOpen() const;
 
 	/**
-	*	@brief
-	*	@pre
-	*	@post
-	*	@return
+	*	@brief	해당하는 id의 Ride의 상세 정보를 출력한다.
+	*	@pre	rideList에 해당하는 id의 Ride가 존재해야함
 	*/
-	void PrintRideInfo();
+	void printRideInfo(int _id) const;
 
 	/**
-	*	@brief
-	*	@pre
-	*	@post
-	*	@return
+	*	@brief	모른 Ride의 대략적인 정보를 출력한다. 
+	*	@pre	rideList에 Ride가 하나라도 존재해야함
 	*/
-	void PrintAllInfo();
+	void printAllRideInfo() const;
 
 	/**
-	*	@brief
+	*	@brief	사용자의 정보 조회?
 	*	@pre
 	*	@post
-	*	@return
+	*	@return	성공시 true 실패시 false
 	*/
-	bool SearchClients();
+	bool searchAllUser();
 
 	/**
-	*	@brief
+	*	@brief	rideList에서 검색한다.
 	*	@pre
 	*	@post
-	*	@return
+	*	@return	성공시 true 실패시 false
 	*/
-	bool SearchRide();
+	bool searchRide();
 
 	/**
-	*	@brief
+	*	@brief	rideList에 Ride를 추가한다.
 	*	@pre
 	*	@post
-	*	@return
+	*	@return	성공시 true 실패시 false
 	*/
 	bool AddRide();
 
 	/**
-	*	@brief
+	*	@brief	rideList의 Ride를 삭제한다.
 	*	@pre
 	*	@post
-	*	@return
+	*	@return	성공시 true 실패시 false
 	*/
 	bool DeleteRide();
 
 	/**
-	*	@brief
+	*	@brief	rideList의 Ride의 정보를 변경한다.
 	*	@pre
 	*	@post
-	*	@return
+	*	@return	성공시 true 실패시 false
 	*/
-	bool UpdateRide();
+	bool updateRide();
 
 	/**
-	*	@brief
+	*	@brief	열렸을 때의 정보를 출력한다.
 	*	@pre
 	*	@post
 	*	@return
 	*/
-	void PrintTodayInfo();
+	void printTodayInfo();
 
 private:
-	int m_Command;
-	int OpenTime;
-	int CloseTime;
-	bool open;
-	Admin admin;
+	int m_Command; //명령어 입력에 사용
+	int OpenTime; //개장한 시간 통계에 사용?
+	int CloseTime; //폐장한 시간 통계에 사용?
+	bool open; //개장 여부
+	Admin admin; //놀이동산 Admin
 };

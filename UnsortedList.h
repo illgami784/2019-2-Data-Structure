12d270cd71abdbec3cefd1678a6359ac6a08e946
bluejoyq <bluejoyq@naver.com>
2019-11-24 22:01:18 +0900

    add UnsortdList

---
+++ UnsortedList.h
@@ -0,0 +1,283 @@
+#ifndef _UNSORTEDLIST_H
+#define _UNSORTEDLIST_H
+
+#include <iostream>
+#include <fstream>	
+#include <string>
+using namespace std;
+
+template <typename T>
+class UnsortedList
+{
+public:
+	/**
+	*	default constructor.
+	*/
+	UnsortedList(int _max)
+	{
+		m_Length = 0;
+		max = _max;
+		m_Array = new T[max];
+		ResetList();
+	}
+
+	/**
+	*	destructor.
+	*/
+	~UnsortedList() 
+	{
+		delete[] m_Array;
+	}
+
+
+	/**
+	*	@brief	Make list empty.
+	*	@pre	none.
+	*	@post	clear list.
+	*/
+	void MakeEmpty();
+
+	/**
+	*	@brief	Get a number of records in current list.
+	*	@pre	none.
+	*	@post	none.
+	*	@return	number of records in current list.
+	*/
+	int GetLength();
+
+	/**
+	*	@brief	Check capacity of list is full.
+	*	@pre	none.
+	*	@post	none.
+	*	@return	return true if list capacity reached to the upper bound, otherwise return false.
+	*/
+	bool IsFull();
+
+	/**
+	*	@brief	add a new data into list.
+	*	@pre	list should be initialized.
+	*	@post	added the new record into the list.
+	*	@param	data	new data.
+	*	@return	return 1 if this function works well, otherwise 0.
+	*/
+	int Add(T data);
+
+	/**
+	*	@brief	Initialize list iterator.
+	*	@pre	list should be initialized.
+	*	@post	iterator is reset.
+	*/
+	void ResetList();
+
+	/**
+	*	@brief	move list iterator to the next item in list and get that item.
+	*	@pre	list and list iterator should not be initialized.
+	*	@post	iterator is moved to next item.
+	*	@param	data	get current iterator's item. it does not need to be initialized.
+	*	@return	index of current iterator's item if is not end of list, oterwise return -1.
+	*/
+	int GetNextItem(T& data);
+
+	/** [작성]
+	*	@brief 입력받은 데이터의 Id가 기존의 존재하는 거랑 같을시 그 아이템의 포인터를 반환
+	*	@pre	아이템이 1개라도 존재해야함.
+	*	@post none
+	*	@param data 서치할 아이템 Id만을 가지면 됨
+	*	@return Id가 같은 아이템의 포인터
+	*/
+	int Get(T& data);
+
+	/** [작성]
+	*	@brief 입력받은 데이터의 시간이 기존의 존재하는 거랑 같을시 그 아이템을 삭제
+	*	@pre 아이템이 하나라도 존재해야함.
+	*	@post 아이템이 하나 삭제됩니다.
+	*	@param data 삭제할 아이템 시간만을 가지면 됨
+	*	@return none
+	*/
+	void Delete(T data);
+
+
+	/** [작성]
+	*	@brief 입력받은 데이터의 시간 사이의 아이템 전부 삭제
+	*	@pre 아이템이 하나라도 존재해야함.
+	*	@post 아이템이 삭제됩니다.
+	*	@param start end 시간만을 가지면됨
+	*	@return none
+	*/
+	void  DeleteTime(T start, T end);
+
+	/** [작성]
+	*	@brief 입력받은 데이터의 Id가 기존의 존재하는 거랑 같을시 그 아이템을 입력받아 갱신
+	*	@pre 아이템이 하나라도 존재해야함
+	*	@post 아이템 레코드를 대체합니다.
+	*	@param data 삭제할 아이템 Id만을 가지면 됨
+	*	@return none
+	*/
+	void Replace(T data);
+
+private:
+	T* m_Array;  ///< list array.
+	int m_Length;				///< number of elements in list.
+	int m_CurPointer;			///< iterator pointer.
+	int max
+};
+#endif
+
+// Make list empty.
+template <typename T>
+void UnsortedList<T>::MakeEmpty()
+{
+	m_Length = 0;
+}
+
+
+// Get a number of records in current list.
+template <typename T>
+int UnsortedList<T>::GetLength()
+{
+	return m_Length;
+}
+
+
+// Check capacity of list is full.
+template <typename T>
+bool UnsortedList<T>::IsFull()
+{
+	if (m_Length > max - 1)
+		return true;
+	else
+		return false;
+}
+
+
+// add a new data into list.
+template <typename T>
+int UnsortedList<T>::Add(T inData)
+{
+	if (IsFull()) {
+		cout << "\n\t리스트가 가득 찼습니다.";
+		return 0;
+	}
+	int location = 0;
+	bool moreToSearch;
+
+	moreToSearch = (location < m_Length);
+	while (moreToSearch) {
+		switch (inData.CompareByTime(m_Array[location]))
+		{
+		case EQUAL:
+			cout << "\n\t동일한 ID가 존재합니다.";
+			return 0;
+		case GREATER:
+			location++;
+			moreToSearch = (location < m_Length);
+			break;
+		case LESS:
+			moreToSearch = false;
+			break;
+		}
+	}
+	cout << "\n\t추가에 성공했습니다.";
+	for (int i = m_Length; i > location; i--) {
+		m_Array[i] = m_Array[i - 1];
+	}
+	m_Array[location] = inData;
+	m_Length++;
+	return 1;
+}
+
+template <typename T>
+int  UnsortedList<T>::Get(T& data) {
+	T temp;
+	ResetList();
+	GetNextItem(temp);
+	while (m_CurPointer < m_Length) {
+		switch (temp.CompareByTime(data))
+		{
+		case EQUAL:
+			data = temp;
+			return 1;
+		case GREATER:
+			return 0;
+		case LESS:
+			GetNextItem(temp);
+			break;
+		}
+	}
+	return 0;
+}
+
+template <typename T>
+void  UnsortedList<T>::Delete(T data) {
+	int location = 0;
+
+	while (data.CompareByTime(m_Array[location]) != EQUAL) {
+		if (location >= m_Length) {
+			cout << "\n\t시간이 같은 값이 없습니다.";
+			return;
+		}
+
+		location++;
+	}
+	for (int i = location + 1; i < max; i++) {
+		m_Array[i - 1] = m_Array[i];
+	}
+	cout << "\n\t삭제에 성공했습니다.";
+	m_Length--;
+}
+
+template <typename T>
+void  UnsortedList<T>::DeleteTime(T start, T end) {
+	T temp;
+	string start_time = start.getTime(), end_time = end.getTime();
+
+	for (int i = max - 1; i > -1; i--) {
+		string temp_time = m_Array[i].getTime();
+		if (temp_time >= start_time && temp_time <= end_time) {
+			cout << "\n\t삭제";
+			if (i == max - 1) m_Length--;
+			else {
+				m_Array[i] = m_Array[i + 1];
+				m_Length--;
+			}
+		}
+	}
+
+
+}
+
+
+template <typename T>
+void  UnsortedList<T>::Replace(T data) {
+	T temp = data;
+	if (Get(data)) {
+		cout << "\n\t" << "대체 성공했습니다.";
+		m_Array[m_CurPointer] = data;
+	}
+	else
+	{
+		cout << "\n\t" << "시간이 같은 값이 없어 대체에 실패했습니다.";
+		return;
+	}
+}
+
+// Initialize list iterator.
+template <typename T>
+void UnsortedList<T>::ResetList()
+{
+	m_CurPointer = -1;
+}
+
+
+// move list iterator to the next item in list and get that item.
+template <typename T>
+int UnsortedList<T>::GetNextItem(T& data)
+{
+	m_CurPointer++;	// list pointer 증가
+	if (m_CurPointer == max)	// end of list이면 -1을 리턴
+		return -1;
+	data = m_Array[m_CurPointer];	// 현재 list pointer의 레코드를 복사
+
+	return m_CurPointer;
+}
+
--- User.cpp
+++ User.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Stack.h"
+#include "User.h"
 #include <iostream>
 using namespace std;
 

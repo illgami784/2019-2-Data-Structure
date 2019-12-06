f2b90f19095cd3ffbc4dd4dfa059d7b397e0a752
rollingman1 <48819383+rollingman1@users.noreply.github.com>
2019-12-06 19:10:15 +0900

    브랜치 병합 충돌

--- Ride.h
+++ Ride.h
@@ -9,6 +9,25 @@
 class User;
 class Admin;
 
+void my_sleep(unsigned msec) {
+	struct timespec req, rem;
+	int err;
+	req.tv_sec = msec / 1000;
+	req.tv_nsec = (msec % 1000) * 1000000;
+	while ((req.tv_sec != 0) || (req.tv_nsec != 0)) {
+		if (nanosleep(&req, &rem) == 0)
+			break;
+		err = errno;
+		// Interrupted; continue
+		if (err == EINTR) {
+			req.tv_sec = rem.tv_sec;
+			req.tv_nsec = rem.tv_nsec;
+		}
+		// Unhandleable error (EFAULT (bad pointer), EINVAL (bad timeval in tv_nsec), or ENOSYS (function not supported))
+		break;
+	}
+}
+
 class Ride{
 public:
 
@@ -139,6 +158,7 @@ bool Ride::rideUser() {//requireTime마다 실행
 		numWaitingUser -= numPerRide;
 	}
 	calcWaitingTime();
+	my_sleep(requireTime);
 
 }
 

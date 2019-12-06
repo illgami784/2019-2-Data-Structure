cdb2cd670f7d3ea2e54916a6238a033392cf85f1
rollingman1 <48819383+rollingman1@users.noreply.github.com>
2019-12-06 19:14:21 +0900

    'm' 브런치 수동 병합

--- Ride.cpp
+++ Ride.cpp
@@ -20,3 +20,27 @@ void Ride::setAllFromKB() {
 int Ride::getMinAge() const {
 	return minAge;
 }
+
+bool Ride::operator<(const Ride &rhs) const {
+	return id < rhs.id;
+}
+
+bool Ride::operator==(const Ride &rhs) const {
+	return id == rhs.id;
+}
+
+bool Ride::operator!=(const Ride &rhs) const {
+	return !(rhs == *this);
+}
+
+bool Ride::operator>(const Ride &rhs) const {
+	return rhs < *this;
+}
+
+bool Ride::operator<=(const Ride &rhs) const {
+	return !(rhs < *this);
+}
+
+bool Ride::operator>=(const Ride &rhs) const {
+	return !(*this < rhs);
+}
